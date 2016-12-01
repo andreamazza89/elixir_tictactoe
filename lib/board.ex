1d2cc898755c404d22f5a14a0131aa84a5090785
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-01 16:01:25 +0000

    Board status now covers all cases. To be refactored

--- lib/board.ex
+++ lib/board.ex
@@ -4,12 +4,30 @@ defmodule Board do
     List.replace_at(board, cell_index, players_mark)
   end
 
-  def status([:x, :x, :x | tail]) do
-    {:win, :x}
+  def status(board) do
+    rows_cols_diags = get_rows(board) ++ get_columns(board) ++ get_diagonals(board)
+    report(rows_cols_diags)
   end
 
-  def status(board) do
-    :incomplete
+  defp get_rows([a,b,c,d,e,f,g,h,i]) do
+    [[a,b,c],[d,e,f],[g,h,i]]
+  end
+
+  defp get_columns([a,b,c,d,e,f,g,h,i]) do
+    [[a,d,g],[b,e,h],[c,f,i]]
+  end
+
+  defp get_diagonals([a,b,c,d,e,f,g,h,i]) do
+    [[a,e,i],[g,e,c]]
+  end
+
+  defp report(rows_cols_diags) do
+    cond do
+      Enum.any?(rows_cols_diags, fn(collection) -> Enum.all?(collection, fn(cell) -> cell === :x end) end) -> {:win, :x}
+      Enum.any?(rows_cols_diags, fn(collection) -> Enum.all?(collection, fn(cell) -> cell === :o end) end) -> {:win, :o}
+      Enum.all?(List.flatten(rows_cols_diags), fn(cell) -> cell !== :empty end) -> :draw
+      true -> :incomplete
+    end
   end
 
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -3,7 +3,10 @@ defmodule BoardTest do
 
   @empty_board [:empty,:empty,:empty,:empty,:empty,:empty,:empty,:empty,:empty]
   @no_winner_no_draw [:x,:empty,:o,:empty,:x,:empty,:empty,:empty,:empty]
-  @crosses_wins [:x,:x,:x,:o,:o,:empty,:empty,:empty,:empty]
+  @crosses_wins_row [:x,:x,:x,:o,:o,:empty,:empty,:empty,:empty]
+  @crosses_wins_column [:x,:empty,:o,:x,:o,:empty,:x,:empty,:empty]
+  @naughts_wins [:o,:o,:o,:x,:x,:empty,:empty,:empty,:empty]
+  @draw [:x,:o,:x,:x,:o,:o,:o,:x,:x]
 
   describe "adding a move to the board" do
     test "updates the board accordingly(example 1)" do
@@ -27,8 +30,21 @@ defmodule BoardTest do
       assert Board.status(@no_winner_no_draw) === :incomplete
     end
 
-    test "a board with a winner is recognised" do
-      assert Board.status(@crosses_wins) === {:win, :x}
+    test "a board with a winner is recognised (crosses wins EXAMPLE 1)" do
+      assert Board.status(@crosses_wins_row) === {:win, :x}
+    end
+
+    test "a board with a winner is recognised (crosses wins EXAMPLE 2)" do
+      assert Board.status(@crosses_wins_column) === {:win, :x}
+    end
+
+    test "a board with a winner is recognised (naughts wins)" do
+      assert Board.status(@naughts_wins) === {:win, :o}
+    end
+
+    test "a draw board is recognised" do
+      assert Board.status(@draw) === :draw
     end
   end
+
 end
