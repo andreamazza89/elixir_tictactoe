5e3dafdf184f6f79bf973e455f32bb73029d7d44
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-06 14:42:09 +0000

    Make Board.Engine.do_play private

--- lib/game/engine.ex
+++ lib/game/engine.ex
@@ -11,7 +11,7 @@ defmodule Game.Engine do
     do_play({player_one_stream, player_one_mark}, {player_two_stream, player_two_mark}, game_state_id)
   end
 
-  def do_play({current_player_stream, current_player_mark}, {next_player_stream, next_player_mark}, state_id) do
+  defp do_play({current_player_stream, current_player_mark}, {next_player_stream, next_player_mark}, state_id) do
     case Game.StateManager.get_board(state_id) |> Board.Evaluation.status() do
       {:win, winner} ->
         IO.puts "The winner was: " <> Atom.to_string(winner)
