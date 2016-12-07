3d71b5fb9260ef9dc1f272450ee38bb2869bf1c6
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-07 10:47:41 +0000

    Remove Game.StateManager and refactor GameEngine to not have single add_move function

--- lib/UI/utilities.ex
+++ lib/UI/utilities.ex
@@ -1,7 +1,7 @@
 defmodule UI.Utilities do
 
-  def get_next_move(game_state_id, input_stream, mark) do
-    IO.puts Board.Presentation.render(Game.StateManager.get_board(game_state_id), [empty: " ", x: "x", o: "o"] )
+  def get_next_move(board, input_stream, mark) do
+    IO.puts Board.Presentation.render(board, [empty: " ", x: "x", o: "o"] )
     IO.gets(input_stream, "\n") |> String.trim |> UI.InputParser.parse_move(mark)
   end
 
--- lib/game/engine.ex
+++ lib/game/engine.ex
@@ -1,25 +1,20 @@
 defmodule Game.Engine do
 
-  #hello side effects!
-  def play_next_move(game_state_id, input_stream, mark) do
-    parsed_move = UI.Utilities.get_next_move(game_state_id, input_stream, mark)
-    Game.StateManager.add_move(game_state_id, parsed_move)
-  end
-
   def play({player_one_stream, player_one_mark}, {player_two_stream, player_two_mark}) do
-    game_state_id = Game.StateManager.start_game
-    do_play({player_one_stream, player_one_mark}, {player_two_stream, player_two_mark}, game_state_id)
+    empty_board = %Board{}
+    do_play({player_one_stream, player_one_mark}, {player_two_stream, player_two_mark}, empty_board)
   end
 
-  defp do_play({current_player_stream, current_player_mark}, {next_player_stream, next_player_mark}, state_id) do
-    case Game.StateManager.get_board(state_id) |> Board.status() do
+  defp do_play({current_player_stream, current_player_mark}, {next_player_stream, next_player_mark}, board) do
+    case Board.status(board) do
       {:win, winner} ->
         IO.puts "The winner was: " <> Atom.to_string(winner)
       :draw ->
         IO.puts "It was a draw!"
       :incomplete ->
-        play_next_move(state_id, current_player_stream, current_player_mark)
-        do_play({next_player_stream, next_player_mark}, {current_player_stream, current_player_mark}, state_id)
+        parsed_move = UI.Utilities.get_next_move(board, current_player_stream, current_player_mark)
+        updated_board = Board.add_move(board, parsed_move)
+        do_play({next_player_stream, next_player_mark}, {current_player_stream, current_player_mark}, updated_board)
     end
   end
 
