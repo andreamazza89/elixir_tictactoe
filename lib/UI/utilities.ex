0dae299eb659d8fc91a22595cb79c1c406c9a632
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-07 11:43:37 +0000

    Move all UI functions under UI.Console + update game engine and tests to reflect this

--- lib/UI/utilities.ex
+++
@@ -1,8 +0,0 @@
-defmodule UI.Utilities do
-
-  def get_next_move(board, input_stream, mark) do
-    IO.puts Board.Presentation.render(board, [empty: " ", x: "x", o: "o"] )
-    IO.gets(input_stream, "\n") |> String.trim |> UI.InputParser.parse_move(mark)
-  end
-
-end
--- test/UI/console_test.exs
+++ test/UI/console_test.exs
@@ -1,16 +1,16 @@
-defmodule UI.InputParserTest do
+defmodule UI.ConsoleTest do
   use ExUnit.Case
 
   test "converts the user's input into a move (example one)" do
-    assert UI.InputParser.parse_move("A1", :x) === {0, :x}
+    assert UI.Console.parse_move("A1", :x) === {0, :x}
   end
 
   test "converts the user's input into a move (example two)" do
-    assert UI.InputParser.parse_move("A1", :o) === {0, :o}
+    assert UI.Console.parse_move("A1", :o) === {0, :o}
   end
 
   test "converts the user's input into a move (example three)" do
-    assert UI.InputParser.parse_move("A2", :o) === {1, :o}
+    assert UI.Console.parse_move("A2", :o) === {1, :o}
   end
 
 end
