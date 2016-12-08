b56f746bb1df22ba7c1fd2dc67e3a6412641772d
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-08 16:33:41 +0000

    Game script now supports humanVcpu by default or humanVhuman optionally

--- lib/play_tictactoe.ex
+++ lib/play_tictactoe.ex
@@ -1,5 +1,12 @@
 defmodule PlayGame do
 
+  def main(["human_v_human"]) do
+    human_player = %Player.Human{mark: :x}
+    linear_cpu_player = %Player.Human{mark: :o}
+    game = %Game{players: {human_player, linear_cpu_player}}
+    UI.Console.play(game)
+  end
+
   def main(_) do
     game = %Game{}
     UI.Console.play(game)
