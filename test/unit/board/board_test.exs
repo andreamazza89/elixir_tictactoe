ed58f2a4910270d7c3c04a11c813793461d40ad6
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-08 15:32:09 +0000

    Implement Board.available_moves

--- test/unit/board/board_test.exs
+++ test/unit/board/board_test.exs
@@ -15,6 +15,30 @@ defmodule BoardTest do
       expected_board = create_board([x: [3], o: []])
       assert Board.add_move(empty_board, {2, :x}) === expected_board
     end
+
+  end
+
+
+  describe "available moves" do
+
+    test "all moves are available for an empty board" do
+      board = create_board([x: [], o: []])
+
+      assert Board.available_moves(board) === [0,1,2,3,4,5,6,7,8]
+    end
+
+    test "some moves are available for a partial board" do
+      board = create_board([x: [1], o: []])
+
+      assert Board.available_moves(board) === [1,2,3,4,5,6,7,8]
+    end
+
+    test "no moves are available for a full board" do
+      board = create_board([x: [1,2,3,4,5], o: [6,7,8,9]])
+
+      assert Board.available_moves(board) === []
+    end
+
   end
 
   
