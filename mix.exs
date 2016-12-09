44b5af3159becc74593f3503fd35f9b40d59daa5
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-12-09 10:50:55 +0000

    Exclude game player script from coveralls

--- mix.exs
+++ mix.exs
@@ -11,7 +11,8 @@ defmodule ElixirRomanNumerals.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
